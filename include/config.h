10532228ab60b0a39f54ed8d8a0952797d962a5d
quangtu19 <quangtuduong9611@gmail.com>
2026-05-23 15:08:01 +0700

    config.h

---
+++ include/config.h
@@ -0,0 +1,13 @@
+cat > include/config.h <<'EOF'
+#ifndef AGENT_CONFIG_H
+#define AGENT_CONFIG_H
+
+struct agent_config {
+    char log_path[256];
+    int heartbeat_interval_sec;
+};
+
+int config_load(const char *path, struct agent_config *cfg);
+
+#endif
+EOF
