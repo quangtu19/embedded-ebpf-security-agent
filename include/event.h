3aadaf9ea6486c77898d04e50374af509aa04fe9
quangtu19 <quangtuduong9611@gmail.com>
2026-05-25 15:01:58 +0700

    Update event.h

--- include/event.h
+++ include/event.h
@@ -5,11 +5,26 @@
 
 #define TASK_COMM_LEN 16
 
+#define EVENT_PROC_EXEC 1
+#define EVENT_TCP_CONNECT 2
+#define EVENT_SUSPICIOUS_CONNECT 3
+
 struct proc_exec_event {
     uint64_t ts_ns;
     uint32_t pid;
     uint32_t uid;
     char comm[TASK_COMM_LEN];
+};
+
+struct tcp_connect_event {
+    uint32_t event_type;
+    uint64_t ts_ns;
+    uint32_t pid;
+    uint32_t uid;
+    char comm[TASK_COMM_LEN];
+    uint32_t dst_ip;
+    uint16_t dst_port;
+    uint8_t protocol;
 };
 
 #endif
