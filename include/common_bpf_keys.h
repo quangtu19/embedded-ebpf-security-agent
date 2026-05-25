301e45830d18f866a69e76ee5ec998131a74e5d6
quangtu19 <quangtuduong9611@gmail.com>
2026-05-25 15:45:08 +0700

    Create common_bpf_keys.h

---
+++ include/common_bpf_keys.h
@@ -0,0 +1,12 @@
+#ifndef COMMON_BPF_KEYS_H
+#define COMMON_BPF_KEYS_H
+
+#define PROTO_TCP    0
+#define PROTO_UDP    1
+#define PROTO_ICMP   2
+#define PROTO_OTHER  3
+
+#define STAT_TOTAL_PACKETS 0
+#define STAT_TOTAL_BYTES   1
+
+#endif
