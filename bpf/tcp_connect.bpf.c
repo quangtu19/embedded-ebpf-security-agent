3bbbfcb40fe745f0e3c9e7658a1d3661ec2e2f44
quangtu19 <quangtuduong9611@gmail.com>
2026-05-26 23:54:05 +0700

    Update tcp_connect.bpf.c

--- bpf/tcp_connect.bpf.c
+++ bpf/tcp_connect.bpf.c
@@ -1,9 +1,9 @@
 #include <linux/bpf.h>
-#include <linux/in.h>
+#include <linux/ptrace.h>
 #include <linux/socket.h>
+#include <linux/in.h>
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_endian.h>
-
 #include "common.bpf.h"
 
 #ifndef AF_INET
@@ -35,22 +35,18 @@ int handle_tcp_connect(struct trace_event_raw_sys_enter *ctx)
     __u16 dport;
 
     uaddr = (void *)ctx->args[1];
-    if (!uaddr) {
+    if (!uaddr)
         return 0;
-    }
 
-    if (bpf_probe_read_user(&addr, sizeof(addr), uaddr) < 0) {
+    if (bpf_probe_read_user(&addr, sizeof(addr), uaddr) < 0)
         return 0;
-    }
 
-    if (addr.sin_family != AF_INET) {
+    if (addr.sin_family != AF_INET)
         return 0;
-    }
 
     e = bpf_ringbuf_reserve(&events, sizeof(*e), 0);
-    if (!e) {
+    if (!e)
         return 0;
-    }
 
     pid_tgid = bpf_get_current_pid_tgid();
     uid_gid = bpf_get_current_uid_gid();
@@ -63,12 +59,10 @@ int handle_tcp_connect(struct trace_event_raw_sys_enter *ctx)
     e->dst_ip = addr.sin_addr.s_addr;
     e->dst_port = dport;
     e->protocol = 6;
-
     bpf_get_current_comm(&e->comm, sizeof(e->comm));
 
-    if (dport == 4444 || dport == 1337 || dport == 31337) {
+    if (dport == 4444 || dport == 1337 || dport == 31337)
         e->event_type = EVENT_SUSPICIOUS_CONNECT;
-    }
 
     bpf_ringbuf_submit(e, 0);
     return 0;
